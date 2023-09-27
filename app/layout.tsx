868041a745674dec03d54469731cac4a8dbf59da
Amr <amruser97@gmail.com>
2023-09-27 23:24:08 +0300

    routing: basic page routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import "./styles/globals.css";
 import "./styles/theme.css";
 import React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next we&apos;re coming!</h1>
-    </div>
-  );
-}
